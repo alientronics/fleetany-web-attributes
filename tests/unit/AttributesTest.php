45bcd50d524b69a4a0fb6ab82e052de579df7935
Rafael <rafaelpviegas@gmail.com>
2016-07-04 22:58:04 -0300

    Teste cobertura de testes

--- tests/unit/AttributesTest.php
+++ tests/unit/AttributesTest.php
@@ -18,10 +18,45 @@ class AttributesTest extends UnitTestCase
             ->once()
             ->andReturn('');
         $mockClient->shouldReceive('request')->andReturn($response);
-
+    
         AttributeRepositoryEloquent::setClient($mockClient);
         $return = AttributeRepositoryEloquent::getAttributesWithValues(1);
+    
+        $this->assertEquals($return, []);
+    }
+    
+    public function testGetAttributesWithValues()
+    {
+        $mockClient = \Mockery::mock('\GuzzleHttp\Client');
+        $response = \Mockery::mock('GuzzleHttp\ResponseInterface');
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn('');
+        $mockClient->shouldReceive('request')->andReturn($response);
+
+        AttributeRepositoryEloquent::setClient($mockClient);
+        $return = AttributeRepositoryEloquent::getAttributesWithValues('vehicle', 1);
 
         $this->assertEquals($return, []);
+    }
+    
+    public function testSetValues()
+    {
+        $mockClient = \Mockery::mock('\GuzzleHttp\Client');
+        $response = \Mockery::mock('GuzzleHttp\ResponseInterface');
+        $response->shouldReceive('getBody')
+            ->once()
+            ->andReturn('');
+        $mockClient->shouldReceive('request')->andReturn($response);
+
+        $inputs = [];
+        $inputs['entity_key'] = 'vehicle';
+        $inputs['entity_id'] = 1;
+        $inputs['attribute1'] = [1];
+        
+        AttributeRepositoryEloquent::setClient($mockClient);
+        $return = AttributeRepositoryEloquent::setValues($inputs);
+
+        $this->assertEquals($return, null);
     }
 }
