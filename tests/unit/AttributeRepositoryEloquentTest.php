6bb8bd2bca20e05fbcc7961240315fc6952c0579
Rafael <rafaelpviegas@gmail.com>
2016-12-17 00:18:19 -0200

    Tentativa de aumento de cobertura

--- tests/unit/AttributeRepositoryEloquentTest.php
+++ tests/unit/AttributeRepositoryEloquentTest.php
@@ -325,28 +325,28 @@ class AttributeRepositoryEloquentTest extends UnitTestCase
         }
     }
     
-//     public function testDownloadFailed()
-//     {
-//         $returnMockClient = [];
-//         $attributeRepo = new AttributeRepositoryEloquent();
-//         $attributeRepo->setClient($this->setGuzzleMock(''));
-//         $return = $attributeRepo->download('dGVzdGUudHh0');
+    public function testDownloadFailed()
+    {
+        $returnMockClient = [];
+        $attributeRepo = new AttributeRepositoryEloquent();
+        $attributeRepo->setClient($this->setGuzzleMock(''));
+        $return = $attributeRepo->download('dGVzdGUudHh0');
 
-//         $this->assertEquals($return, null);
-//     }
+        $this->assertEquals($return, null);
+    }
     
-//     public function testDownloadSuccess()
-//     {
-//         $object = new \stdClass();
-//         $object->name = 'name';
-//         $object->file = 'file';
-//         $object->mimetype = 'mimetype';
-//         $returnMockClient[] = $object;
+    public function testDownloadSuccess()
+    {
+        $object = new \stdClass();
+        $object->name = 'name';
+        $object->file = 'file';
+        $object->mimetype = 'mimetype';
+        $returnMockClient[] = $object;
         
-//         $attributeRepo = new AttributeRepositoryEloquent();
-//         $attributeRepo->setClient($this->setGuzzleMock(json_encode($returnMockClient)));
-//         $return = $attributeRepo->download('dGVzdGUudHh0');
+        $attributeRepo = new AttributeRepositoryEloquent();
+        $attributeRepo->setClient($this->setGuzzleMock(json_encode($returnMockClient)));
+        $return = $attributeRepo->download('dGVzdGUudHh0');
 
-//         $this->assertInstanceOf(is_object($return), true);
-//     }
+        $this->assertInstanceOf(is_object($return), true);
+    }
 }
